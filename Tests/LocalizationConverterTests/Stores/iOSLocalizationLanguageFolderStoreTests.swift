80505f031268722f89d332f76f776f7174d9576b
Liquidsoul <liquidsoul@liquidsoul.fr>
2016-09-30 23:02:02 +0200

    Add iOSLocalizationLanguageFolderStoreTests

---
+++ Tests/LocalizationConverterTests/Stores/iOSLocalizationLanguageFolderStoreTests.swift
@@ -0,0 +1,26 @@
+//
+//  iOSLocalizationLanguageFolderStoreTests.swift
+//
+//  Created by Sébastien Duperron on 26/09/2016.
+//  Copyright © 2016 Sébastien Duperron
+//  Released under an MIT license: http://opensource.org/licenses/MIT
+//
+
+import XCTest
+
+@testable import LocalizationConverter
+
+class iOSLocalizationLanguageFolderStoreTests: XCTestCase {
+
+    func test_That_store_ReturnsTheCorrectStoreForAGivenLanguage() {
+        // GIVEN: a language folder store
+        let languageFolderStore = iOSLocalizationLanguageFolderStore(folderPath: "outFolder", includePlurals: false)
+
+        // WHEN: we retrieve to store for the base language
+        let store = languageFolderStore.store(for: .base)
+
+        // THEN: we have received the expected store
+        XCTAssertTrue(store is iOSLocalizationFileStore)
+    }
+
+}
