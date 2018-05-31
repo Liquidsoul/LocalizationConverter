70c3fb85545eb4213f9e8705c2e948c753db0adc
Liquidsoul <liquidsoul@liquidsoul.fr>
2018-05-31 10:58:55 +0200

    Fix output_dict name to outputDict

--- Sources/LocalizationConverter/Parsers/LocalizableParser.swift
+++ Sources/LocalizationConverter/Parsers/LocalizableParser.swift
@@ -13,15 +13,15 @@ class LocalizableParser: LocalizationParser {
         return string.characters
             .split { $0 == "\n" }
             .reduce(LocalizationMap(format: .ios), { (localizationDict, keyValueCharView) -> LocalizationMap in
-                var output_dict = localizationDict
+                var outputDict = localizationDict
                 let trimCharacterSet = CharacterSet(charactersIn: " \n\";")
                 let array = keyValueCharView
                     .split { $0 == "="}
                     .map { String.init($0).trimmingCharacters(in: trimCharacterSet) }
                 if array.count == 2 {
-                    output_dict[array[0]] = .string(value: array[1])
+                    outputDict[array[0]] = .string(value: array[1])
                 }
-                return output_dict
+                return outputDict
             })
     }
 }
